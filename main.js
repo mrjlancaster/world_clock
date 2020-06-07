f185350a5642c841a0edb7402ca6abd82bbce530
mrjlancaster <lancast001@gmail.com>
2020-06-07 17:15:05 -0500

    change timezone

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-//clock
-function currentTime() {
+const currentTimezone = new Date();
+
+
+//---------------------
+//   UTC TIMEZONE
+//---------------------
+
+function newYorkTime() {
     //store current date/time in a variable
     const time = new Date();
 
@@ -12,4 +18,47 @@ function currentTime() {
     document.querySelector('.clock').innerHTML = hour + ':' + mins + ':' + sec;
 }
 
+setInterval(newYorkTime, 1000);
+
+
+
+
+
+//---------------------
+//   UTC TIMEZONE
+//---------------------
+
+const utc_timezone = timeNow.getTimezoneOffset();
+
+// console.log('UTC offset: ' + utc_timezone);
+
+timeNow.setMinutes(timeNow.getMinutes() + utc_timezone);
+
+// console.log('UTC: ' + timeNow);
+
+
+
+
+
+
+
+//---------------------
+//   LONDON TIME
+//---------------------
+
+
+function currentTime() {
+    //store current date/time in a variable
+    const time = new Date();
+
+    //get current time
+    const hour = time.getHours();
+    const mins = time.getMinutes();
+    const sec = time.getSeconds();
+    
+    //add time to document
+    document.querySelector('.london_clock').innerHTML = hour + ':' + mins + ':' + sec;
+}
+
 setInterval(currentTime, 1000);
+
